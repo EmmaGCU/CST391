fb09b370084930869fe0a135921f878a4f547fb7
honeyishrunkthekids <emmagostling@msn.com>
2025-03-09 23:51:44 -0400

    week 1 end

--- Week1/MusicAPI/src/app.ts
+++ Week1/MusicAPI/src/app.ts
@@ -2,12 +2,23 @@ import express from 'express'; //imports necessary for use within the file
 import albumsRouter from './albums/albums.routes';
 import artistsRouter from './artists/artists.routes';
 import logger from './middleware/logger.middleware';
+import dotenv from "dotenv";
+import cors from 'cors';
+import helmet from 'helmet';
+
+dotenv.config();
 
 const app = express(); //initialize the application
 const port = 3000; //define the port number
 
 // Make sure you understand the following line of code.
 app.use('/', [albumsRouter, artistsRouter]);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(cors());
+app.use(helmet());
 
 app.listen(port, () => {
     console.log('Example app listening at http://localhost:${port}') //Write a message to the console
